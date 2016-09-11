f73e20d1727bd065074fed54430c86f1ebeeafc8
Shunsuke Yamazaki <sa@shunsuke-no-MacBook-Air.local>
2016-09-12 07:40:28 +0900

    Change test/ to spec/.

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :task do
+    user_id 1
+    name "MyString"
+    description "MyString"
+    state "MyString"
+    created_at "2016-09-10 19:56:19"
+    updated_at "2016-09-10 19:56:19"
+  end
+end
