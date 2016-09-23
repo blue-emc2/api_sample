945a78d7628c24b0fd78f6568102aaa9bb12c843
Shunsuke Yamazaki <sa@shunsuke-no-MacBook-Air.local>
2016-09-24 08:36:20 +0900

    make create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,16 @@ class TasksController < ActionController::API
     @tasks ||= Task.all
     render json: @tasks
   end
+
+  def create
+    @task = Task.new(task_param)
+    @task.save!
+    render json: @task.id
+  end
+
+  private
+
+  def task_param
+    params.require(:task).permit(:name, :description, :user_id)
+  end
 end
