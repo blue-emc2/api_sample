26d114670691dc17fd8ef4a51b176901deb606cf
Shunsuke Yamazaki <sa@shunsuke-no-MacBook-Air.local>
2016-10-02 21:46:45 +0900

    Add create_ordered_all.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,22 @@
 class TasksController < ActionController::API
 
   def index
-    @tasks ||= Task.all
-    render json: @tasks
+    render json: create_ordered_all
   end
 
   def create
-    @task = Task.new(task_param)
-    @task.save!
-    render json: @task.id
+    task = Task.new(task_param)
+    task.save!
+    render json: create_ordered_all
   end
 
   private
 
   def task_param
     params.require(:task).permit(:name, :description, :user_id)
+  end
+
+  def create_ordered_all
+    Task.all.order(created_at: :desc)
   end
 end
